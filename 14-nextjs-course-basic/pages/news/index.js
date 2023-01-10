f0744789b99378fece3db10a8b2114e2aeccd6d3
sancho-sumy <www.sancho@gmail.com>
2023-01-10 15:04:48 +0200

    add: next.js course basic

---
+++ 14-nextjs-course-basic/pages/news/index.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import { Fragment } from 'react';
+
+function NewsPage() {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-us-a-great-framework">NextJS Is A Great Framework</Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default NewsPage;
